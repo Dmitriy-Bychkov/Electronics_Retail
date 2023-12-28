9d71eb84c10fcf12374b9d641ec4c78b9c349bc0
Dmitriy Bychkov <582620@gmail.com>
2023-12-28 18:13:22 +0800

    Add user's presentation in the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """ Представление пользователей в админке """
+
+    list_display = (
+        'first_name',
+        'last_name',
+        'email',
+    )
+    search_fields = (
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'country',
+    )
