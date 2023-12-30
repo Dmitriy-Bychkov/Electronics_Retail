0fec8c84f34c77ad54fa57685e3d7900160b1996
Dmitriy Bychkov <582620@gmail.com>
2023-12-30 12:49:37 +0800

    Add an additional field "is_active"

--- users/models.py
+++ users/models.py
@@ -13,6 +13,7 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='users/', default='no_image.jpg', verbose_name='аватар', **NULLABLE)
     phone = models.CharField(unique=True, max_length=20, verbose_name='телефон', **NULLABLE)
     country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
+    is_active = models.BooleanField(default=False, verbose_name='статус активности')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
