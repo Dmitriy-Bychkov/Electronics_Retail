5b919484f33747bf6611f505ea8893ff1f91284b
Dmitriy Bychkov <582620@gmail.com>
2023-12-30 21:42:56 +0800

    Make some changes according to PEP8

--- users/models.py
+++ users/models.py
@@ -10,15 +10,20 @@ class User(AbstractUser):
     username = None
 
     email = models.EmailField(unique=True, verbose_name='почта')
-    avatar = models.ImageField(upload_to='users/', default='no_image.jpg', verbose_name='аватар', **NULLABLE)
-    phone = models.CharField(unique=True, max_length=20, verbose_name='телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', default='no_image.jpg',
+                               verbose_name='аватар', **NULLABLE)
+    phone = models.CharField(unique=True, max_length=20, verbose_name='телефон',
+                             **NULLABLE)
     country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
-    is_active = models.BooleanField(default=False, verbose_name='статус активности')
+    is_active = models.BooleanField(default=False,
+                                    verbose_name='статус активности')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     def __str__(self):
+        """ Строковое отображение объекта """
+
         return f'{self.first_name} - {self.email}'
 
     class Meta:
--- users/permissions.py
+++ users/permissions.py
@@ -2,7 +2,10 @@ from rest_framework.permissions import BasePermission
 
 
 class IsActiveUser(BasePermission):
-    """ Настройка прав, чтобы только активные пользователи имели доступ к API """
+    """
+    Настройка прав, чтобы только активные
+    пользователи имели доступ к API
+    """
 
     def has_object_permission(self, request, view, obj):
         if request.user.is_active:
