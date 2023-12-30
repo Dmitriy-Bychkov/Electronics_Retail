e1e7e7467f195037ac5930a0d50d6ec69c325bca
Dmitriy Bychkov <582620@gmail.com>
2023-12-30 12:53:55 +0800

    Add all user's controllers and its urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import generics
+from users.models import User
+from users.permissions import IsActiveUser
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class UserCreateAPIView(generics.CreateAPIView):
+    """ Создание пользователя """
+
+    serializer_class = UserSerializer
+    permission_classes = [IsActiveUser]
+
+
+class UserListAPIView(generics.ListAPIView):
+    """ Вывод списка пользователей """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsActiveUser]
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    """ Вывод одного пользователя """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsActiveUser]
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    """ Редактирование пользователя """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsActiveUser]
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    """ Удаление пользователя """
+
+    queryset = User.objects.all()
+    permission_classes = [IsActiveUser]
